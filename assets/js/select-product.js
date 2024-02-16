eb5aead8634e65bfe8cee8f87ebe35b9eeb83656
TerrieL <60408333+terrielama@users.noreply.github.com>
2024-02-16 17:10:26 +0100

    Update select-product.js

--- assets/js/select-product.js
+++ assets/js/select-product.js
@@ -25,90 +25,61 @@ productThumbs.forEach((productThumb,index) => {
         productDiv.className = `product active-${index + 1}`
     })
 });
-// La propriété classList est utilisée pour représenter la valeur des éléments de classe qui est un objet DOMTokenList .
-// DOMContentLoaded, qui se déclenche lorsque le document HTML a été complètement chargé
+
+// ---------------------------------------------------------------
+
 document.addEventListener('DOMContentLoaded', function () {
-  
-    swatches.forEach((button) => {
-      button.addEventListener('click', function () {
-        swatches.forEach((btn) => btn.classList.remove('selected'));
-        this.classList.add('selected');
+  swatches.forEach((button) => {
+    button.addEventListener('click', function () {
+      swatches.forEach((btn) => btn.classList.remove('selected'));
+      this.classList.add('selected');
 
-        
-        switch (true) {
-//  contains est une méthode de l'objet classList. Elle est utilisée pour vérifier si une classe particulière est présente dans la liste des classes de l'élément HTML.
-            case this.classList.contains('btn-red'):
-                // forEach permet d'exécuter une fonction pour chaque élément.
-              productThumbs.forEach((thumb) => {
-// currentImageSrc : constante appelée en extrayant la propriété src de l'objet thumb
-                const currentImageSrc = thumb.src;
-
-// remplacer dans le chemin de l'image toutes les occurrences de 'nike-pegasus-"couleur"' par 'nike-pegasus-"autre couleur"'
-                const newImageSrc = currentImageSrc.replace('nike-pegasus-green', 'nike-pegasus-red');
-                thumb.src = newImageSrc;
-              });
-              break;
-             }
-
-             switch (true) {
-// La méthode contain() est utilisée pour vérifier si la classe spécifiée existe dans les classes CSS et par rapport à elle, elle renvoie la valeur booléenne comme vraie ou fausse.
-                case this.classList.contains('btn-red'):
-                  productThumbs.forEach((thumb) => {
-                    const currentImageSrc = thumb.src;
-                    const newImageSrc = currentImageSrc.replace('nike-pegasus-purple', 'nike-pegasus-red');
-                    
-                    thumb.src = newImageSrc;
-                  });
-                  break;
-                 }
+// this fait référence à l'élément DOM sur lequel l'événement de clic a été déclenché, c'est-à-dire le bouton.
+// dataset est un objet qui représente tous les attributs de données (data-*) de l'élément. L'attribut data-color est alors extrait en utilisant dataset.color.
+      const color = this.dataset.color;
 
 
-             switch (true) {
-                case this.classList.contains('btn-purple'):
-                  productThumbs.forEach((thumb) => {
-                    const currentImageSrc = thumb.src;
-                    const newImageSrc = currentImageSrc.replace('nike-pegasus-red', 'nike-pegasus-purple');
-                    
-                    thumb.src = newImageSrc;
-                  });
-                  break;
-                 }
+// productThumbs, qui représente toutes les images des miniatures du produit.
+// productThumbs.forEach((thumb) => { ... }); : Cela signifie que pour chaque élément thumb dans productThumbs, le code à l'intérieur des accolades sera exécuté.
+// const currentImageSrc = thumb.src; : Cette ligne extrait la source (l'URL) de l'image actuellement pointée par l'itération de la boucle et la stocke dans la variable currentImageSrc.
+// let newColor; : Cette ligne déclare une variable newColor en utilisant le mot-clé let, qui sera utilisée pour stocker la nouvelle couleur à appliquer à l'image. Cependant, à ce stade, la variable n'a pas encore de valeur.
+      productThumbs.forEach((thumb) => {
+        const currentImageSrc = thumb.src;
+        let newColor;
 
-                 switch (true) {
-                    case this.classList.contains('btn-purple'):
-                      productThumbs.forEach((thumb) => {
-                        const currentImageSrc = thumb.src;
-                        const newImageSrc = currentImageSrc.replace('nike-pegasus-green', 'nike-pegasus-purple');
-                        
-                        thumb.src = newImageSrc;
-                      });
-                      break;
-                     }
+        switch (color) {
+          case 'red':
+            newColor = 'red';
+            break;
+          case 'purple':
+            newColor = 'purple';
+            break;
+          case 'green':
+            newColor = 'green';
+            break;
+          default:
+            newColor = 'green'; // Couleur par défaut
+        }
 
-                 switch (true) {
-                    case this.classList.contains('btn-green'):
-                      productThumbs.forEach((thumb) => {
-                        const currentImageSrc = thumb.src;
-                        const newImageSrc = currentImageSrc.replace('nike-pegasus-purple', 'nike-pegasus-green');
-                        
-                        thumb.src = newImageSrc;
-                      });
-                      break;
-                     }
-
-                     
-                 switch (true) {
-                    case this.classList.contains('btn-green'):
-                      productThumbs.forEach((thumb) => {
-                        const currentImageSrc = thumb.src;
-                        const newImageSrc = currentImageSrc.replace('nike-pegasus-red', 'nike-pegasus-green');
-                        
-                        thumb.src = newImageSrc;
-                      });
-                      break;
-                     }
-        });
+// const newImageSrc = currentImageSrc.replace(nike-pegasus-${color}, nike-pegasus-${newColor}); : Dans cette ligne, la méthode replace() est utilisée pour effectuer le remplacement de texte dans la chaîne currentImageSrc. La partie à remplacer est déterminée par l'expression  ${color}, où ${color} est la couleur actuelle extraite des données de l'élément sur lequel l'itération de la boucle est en cours.
+// Cette partie du chemin de l'image est remplacée par la nouvelle couleur, représentée par ${newColor}. Ainsi, cette ligne crée un nouveau chemin d'image avec la couleur mise à jour.
+// thumb.src = newImageSrc; : Cette ligne met à jour la propriété src de l'élément de l'image (thumb) avec le nouveau chemin d'image. elle applique le chemin d'image modifié à l'image miniature, changeant ainsi dynamiquement la couleur de la miniature en fonction de l'interaction de l'utilisateur.
+        const newImageSrc = currentImageSrc.replace(`nike-pegasus-${color}`, `nike-pegasus-${newColor}`);
+        thumb.src = newImageSrc;
       });
+
+      //  l'image principale du produit.
+// const currentMainImageSrc = productMainImg.src; : Cela récupère le chemin actuel de l'image principale du produit en le stockant dans la variable currentMainImageSrc.
+// const newMainImageSrc = currentMainImageSrc.replace(nike-pegasus-${currentColor}, nike-pegasus-${color}); : La méthode replace() est utilisée pour effectuer le remplacement de texte dans la chaîne currentMainImageSrc. La partie à remplacer est déterminée par l'expression régulière ${currentColor}, où ${currentColor} est la couleur actuelle de l'image principale, stockée précédemment. Cette partie du chemin de l'image principale est remplacée par la nouvelle couleur, représentée par ${color}. cette ligne crée un nouveau chemin d'image pour l'image principale avec la couleur choisi.
+// productMainImg.src = newMainImageSrc; : Cette ligne met à jour la propriété src de l'élément de l'image principale (productMainImg) avec le nouveau chemin d'image. elle applique le chemin d'image modifié à l'image principale, changeant ainsi dynamiquement la couleur de l'image principale en fonction de l'interaction de l'utilisateur.
+      const currentMainImageSrc = productMainImg.src;
+      const newMainImageSrc = currentMainImageSrc.replace(`nike-pegasus-${currentColor}`, `nike-pegasus-${color}`);
+      productMainImg.src = newMainImageSrc;
+
+      // productDiv.className = `product active-${color}`; : Ici, la propriété className de l'objet productDiv est modifiée. La nouvelle valeur de la classe est définie à l'aide d'une chaîne de texte dynamique générée à l'aide d'interpolation. La classe mise à jour est composée de deux parties 
+// 'product' est classe statique qui est utilisée pour définir les styles de base communs à tous les produits.
+// active-${color} : C'est une classe dynamique qui dépend de la couleur actuelle sélectionnée par l'utilisateur. Elle est générée en fonction de la couleur choisie (color). Ainsi, cette partie de la classe varie dynamiquement en fonction de la couleur sélectionnée, permettant ainsi de styliser différemment les produits en fonction de leur couleur.
+      productDiv.className = `product active-${color}`;
     });
-
-
+  });
+});
